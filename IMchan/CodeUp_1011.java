0d3d57412e7269e309186548a0fa8464556fa1d4
김찬영 <rlacksdud3@hanmail.net>
2019-10-01 13:06:59 +0900

    Create CodeUp_1011.java

---
+++ IMchan/CodeUp_1011.java
@@ -0,0 +1,14 @@
+package 코드업;
+
+import java.util.Scanner;
+
+public class CodeUp_1011 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		 double n = sc.nextDouble(); //문제 이상한데?..
+		System.out.format("%f",n); // float로 변수를 선언하라고해서 했는데 ..
+	}							//double로 쓰고 format을 "%f" 형식을 바꿔준다
+								//그리고 , 변수);
+}
